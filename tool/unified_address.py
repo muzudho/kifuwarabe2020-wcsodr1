f387ab322ba2107ce11c17c6d9bac9c2143cf579
むずでょ <muzudho1@gmail.com>
2020-06-11 19:59:55 +0900

    WIP

--- tool/unified_address.py
+++ tool/unified_address.py
@@ -2,9 +2,9 @@ print('trace   | Start.')
 print('pub enum UnifiedAddress {')
 for file in range(1, 10):
     for rank in range(1, 10):
-        print(f'    sq{file}{rank},')
+        print(f'    Sq{file}{rank},')
 for phase in range(1, 3):
-    for hand in ['king', 'rook', 'bishop', 'gold', 'silver', 'knight', 'lance', 'pawn']:
+    for hand in ['King', 'Rook', 'Bishop', 'Gold', 'Silver', 'Knight', 'Lance', 'Pawn']:
         print(f'    {hand}{phase},')
 print('}')
 print('trace   | Finished.')
