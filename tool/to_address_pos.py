4d2e977aa2cfdbceadf68dd6dfc79e69f9d9d732
むずでょ <muzudho1@gmail.com>
2020-06-11 20:17:26 +0900

    WIP

---
+++ tool/to_address_pos.py
@@ -0,0 +1,12 @@
+print('trace   | Start.')
+print('const map: [AddressPos; 178] = [')
+for phase in range(1, 3):
+    for file in range(1, 10):
+        for rank in range(1, 10):
+            print(
+                f'    AddressPos::Board(AbsoluteAddress2D {{ file: {file}, rank: {rank} }}),')
+for phase in range(1, 3):
+    for hand in ['King', 'Rook', 'Bishop', 'Gold', 'Silver', 'Knight', 'Lance', 'Pawn']:
+        print(f'    AddressPos::Hand(DoubleFacedPiece::{hand}{phase}),')
+print('];')
+print('trace   | Finished.')
