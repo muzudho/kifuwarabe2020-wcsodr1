3b0a401ce8d2b8c233f76b58213bae8f710b1568
むずでょ <muzudho1@gmail.com>
2020-07-19 22:30:49 +0900

    remove unwrap

--- src/config.rs
+++ src/config.rs
@@ -3,7 +3,7 @@
 //!
 
 /// USI対応コンピューター将棋ソフトの名前☆（＾～＾）
-pub const ENGINE_NAME: &str = "KifuwarabeDR1 bld269";
+pub const ENGINE_NAME: &str = "KifuwarabeDR1 bld271";
 
 /// 作者の名前。姓・名の順にしたいぜ☆（＾～＾）異文化に通じる表記方法はないものか☆（＾～＾）
 pub const ENGINE_AUTHOR: &str = "TAKAHASHI, Satoshi";
@@ -14,4 +14,4 @@ pub const LOG_FILE: &str = "kifuwarabe2020_dr1";
 pub const PV_BUFFER: usize = 2048;
 
 /// info 表示の間隔（ミリ秒）
-pub const INFO_INTERVAL_MSEC: u128 = 100;
+pub const INFO_INTERVAL_MSEC: u128 = 1000;
--- src/spaceship/engine.rs
+++ src/spaceship/engine.rs
@@ -1,4 +1,6 @@
 use crate::command_line_seek::CommandLineSeek;
+use crate::log::LogExt;
+use casual_logger::Log;
 use regex::Regex;
 use std::fmt;
 
@@ -22,13 +24,28 @@ impl Go {
     /// -------
     /// go btime 40000 wtime 50000 binc 10000 winc 10000
     pub fn parse(p: &mut CommandLineSeek) -> Go {
-        let re = Regex::new(r"^go btime (\d+) wtime (\d+) binc (\d+) winc (\d+)$").unwrap();
+        let re = match Regex::new(r"^go btime (\d+) wtime (\d+) binc (\d+) winc (\d+)$") {
+            Result::Ok(val) => val,
+            Result::Err(e) => panic!(Log::print_fatal(&format!("Invalid regex=|{}|", e))),
+        };
         if let Some(cap) = re.captures(p.line()) {
             Go {
-                btime: cap[1].parse().unwrap(),
-                wtime: cap[2].parse().unwrap(),
-                binc: cap[3].parse().unwrap(),
-                winc: cap[4].parse().unwrap(),
+                btime: match cap[1].parse() {
+                    Result::Ok(val) => val,
+                    Result::Err(e) => panic!(Log::print_fatal(&format!("Invalid cap1=|{}|", e))),
+                },
+                wtime: match cap[2].parse() {
+                    Result::Ok(val) => val,
+                    Result::Err(e) => panic!(Log::print_fatal(&format!("Invalid cap2=|{}|", e))),
+                },
+                binc: match cap[3].parse() {
+                    Result::Ok(val) => val,
+                    Result::Err(e) => panic!(Log::print_fatal(&format!("Invalid cap3=|{}|", e))),
+                },
+                winc: match cap[4].parse() {
+                    Result::Ok(val) => val,
+                    Result::Err(e) => panic!(Log::print_fatal(&format!("Invalid cap4=|{}|", e))),
+                },
             }
         } else {
             // デバッグ時に `go` のみ打鍵した場合など。小さな数にします。
